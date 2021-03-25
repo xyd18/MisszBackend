740b91702c547b27d4a656f26944b0ef3033a712
xyd18 <xyd18@mails.tsinghua.edu.cn>
2021-03-25 17:45:59 +0800

    fix run.sh

--- run.sh
+++ run.sh
@@ -1,5 +1,7 @@
 cd missz
 #python3 manage.py makemigrations dream
 #python3 manage.py migrate
-pip install -r ../requirements.txt
+pip3 install -r requirements.txt
+echo "finish install requirements.txt"
 nohup python3 manage.py runserver 0.0.0.0:8000 > backend.log 2>&1 &
+echo "runserver"

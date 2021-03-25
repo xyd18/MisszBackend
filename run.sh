f997f11551a54d4721d8fd61a08a977cb324796e
xyd18 <xyd18@mails.tsinghua.edu.cn>
2021-03-25 17:35:31 +0800

    nohup

--- run.sh
+++ run.sh
@@ -2,4 +2,4 @@ cd missz
 #python3 manage.py makemigrations dream
 #python3 manage.py migrate
 pip install -r ../requirements.txt
-python3 manage.py runserver 0.0.0.0:8000
+nohup python3 manage.py runserver 0.0.0.0:8000 > backend.log 2>&1 &

da6841e74b594a7a5f026919026a18d3aaf7dc92
xyd18 <xyd18@mails.tsinghua.edu.cn>
2021-03-25 21:45:32 +0800

    change url

--- missz/dream/urls.py
+++ missz/dream/urls.py
@@ -3,6 +3,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('/user', views.check_times, name='check_times'),
+    path('user/', views.check_times, name='check_times'),
     path('', views.interpret_dream, name='interpret_dream')
 ]

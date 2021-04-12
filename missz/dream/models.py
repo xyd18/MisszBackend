ff0b8638537e54b4f870cb3b262f3144d408e438
Tiantian Wu <chen970919561@qq.com>
2021-04-12 18:17:16 +0800

    support a simple database

--- missz/dream/models.py
+++ missz/dream/models.py
@@ -1,3 +1,7 @@
 from django.db import models
 
 # Create your models here.
+class dreamEntry(models.Model):
+    dream = models.TextField(primary_key=True)
+    interpret = models.TextField()
+    sentence_embedding = models.TextField()

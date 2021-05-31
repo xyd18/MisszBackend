2c8a8ff500478f739b44a94c62de433e49718496
xyd18 <xyd18@mails.tsinghua.edu.cn>
2021-05-31 18:45:01 +0800

    compute_all_embeddings

--- missz/dream/utils.py
+++ missz/dream/utils.py
@@ -1,12 +1,13 @@
 from sentence_transformers import SentenceTransformer, util
 from dream.models import dreamEntry
+from dream.db import insert_db
 
 TOKEN = "c486e142f0ed6050f8d0497968079eb2"
 TOKEN_PUB = "da5d90ab00b50c2ba032d944d75f95db"
 URL_USER = "http://lab.aminer.cn/isoa-2021/user"
 URL_GPT = "http://lab.aminer.cn/isoa-2021/gpt"
 
-
+model = SentenceTransformer('paraphrase-TinyBERT-L6-v2')
 
 def str2list(string):
 	strlist = string.split(",")
@@ -18,10 +19,9 @@ def embed2str(embed):
 	return ",".join(strlist)
 
 def compute_all_embeddings():
-	model = SentenceTransformer('paraphrase-TinyBERT-L6-v2')
-
 	for d in dreamEntry.objects.all():
 		embedding = model.encode(d.dream)
+		insert_db(d.dream, d.interpret, embed2str(embedding), d.good_num, d.bad_num)
 
 
 def get_similar_dream(dream_embedding):

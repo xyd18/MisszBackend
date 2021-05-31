6e368d67e137e0433171dc70462e6dcb7633d7c1
Tiantian Wu <chen970919561@qq.com>
2021-05-31 16:47:41 +0800

    add id as the primary key in database

--- missz/dream/models.py
+++ missz/dream/models.py
@@ -2,7 +2,8 @@ from django.db import models
 
 # Create your models here.
 class dreamEntry(models.Model):
-    dream = models.TextField(primary_key=True)
+    id = models.AutoField(primary_key=True)
+    dream = models.TextField()
     interpret = models.TextField()
     sentence_embedding = models.JSONField(null=True)
     good_num = models.BigIntegerField(default=0)

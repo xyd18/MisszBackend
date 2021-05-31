b778e3b1c0a2b11e3061b6feb0437775267a5cff
Tiantian Wu <chen970919561@qq.com>
2021-06-01 00:12:47 +0800

    add thumb up/down function

--- missz/dream/db.py
+++ missz/dream/db.py
@@ -2,7 +2,7 @@ from . import models
 from django.http import JsonResponse
 
 
-def insert_db(dream, interpret, sent_embed, good_num, bad_num):
+def insert_db(dream, interpret, sent_embed, good_num=0, bad_num=0):
     dreams_in_db = models.dreamEntry.objects.filter(dream=dream)
     if dreams_in_db.count() == 0:
         models.dreamEntry.objects.create(dream=dream, interpret=interpret, sentence_embedding=sent_embed, good_num=good_num, bad_num=bad_num)
@@ -16,7 +16,7 @@ def delete_db(dream):
 
 def get_db(dream):
     dream_in_db = models.dreamEntry.objects.get(dream=dream)
-    return dream_in_db.interpret
+    return dream_in_db.interpret, dream_in_db.sentence_embedding, dream_in_db.good_num, dream_in_db.bad_num
 
 
 def ask_db(dream):

71f6264a55278fe368fc45c326741e26fb9dd7af
xyd18 <xyd18@mails.tsinghua.edu.cn>
2021-05-31 15:23:45 +0800

    整理文件

--- missz/dream/urls.py
+++ missz/dream/urls.py
@@ -5,5 +5,6 @@ from . import views
 urlpatterns = [
     path('user/', views.check_times, name='check_times'),
     path('all/', views.all_dream, name='all_dream'),
+    path('similar/', views.similar_dream, name='similar_dream'),
     path('', views.interpret_dream, name='interpret_dream')
 ]
--- missz/dream/views.py
+++ missz/dream/views.py
@@ -1,9 +1,7 @@
 # Create your views here.
 from django.http import HttpResponse
-from django.http import JsonResponse
 
-from . import models
-from . import tasks
+from . import tasks, db, utils
 # import asyncio
 import _thread
 
@@ -12,49 +10,8 @@ import json
 import logging
 import re
 
-TOKEN = "c486e142f0ed6050f8d0497968079eb2"
-TOKEN_PUB = "da5d90ab00b50c2ba032d944d75f95db"
-URL_USER = "http://lab.aminer.cn/isoa-2021/user"
-URL_GPT = "http://lab.aminer.cn/isoa-2021/gpt"
-
-
-def insert_db(dream, interpret, sent_embed):
-    dreams_in_db = models.dreamEntry.objects.filter(dream=dream)
-    if dreams_in_db.count() == 0:
-        models.dreamEntry.objects.create(dream=dream, interpret=interpret, sentence_embedding=sent_embed)
-    else:
-        dreams_in_db.update(interpret=interpret)
-
-
-def delete_db(dream):
-    models.dreamEntry.object.filter(dream=dream).delete()
-
-
-def get_db(dream):
-    dream_in_db = models.dreamEntry.objects.get(dream=dream)
-    return dream_in_db.interpret
-
-
-def ask_db(dream):
-    dreams_in_db = models.dreamEntry.objects.filter(dream=dream)
-    if dreams_in_db.count() == 0:
-        return False
-    return True
-
-
-def get_all_db():
-    data_list = models.dreamEntry.objects.all()
-    info = []
-    for data in data_list:
-        dream = data.dream
-        interpret = data.interpret
-        meta_dict = {"dream": dream, "interpret": interpret}
-        info.append(meta_dict)
-    json_data = {"data": info}
-    return JsonResponse(json_data, status=200)
-
 def all_dream(request):
-    return get_all_db()
+    return db.get_all_db()
 
 def deduplication(txt):
     lines = re.split(r"([.。!！?？；;：:，,\s+])", txt)
@@ -116,13 +73,13 @@ def interpret_dream(request):
         print(return_json)
         return HttpResponse(return_json)
 
-    if ask_db(dream):
-        return HttpResponse(get_db(dream))
+    if db.ask_db(dream):
+        return HttpResponse(db.get_db(dream))
 
     # content = "身份：军人。年龄：25岁。性别：女。梦境：" + dream + "周公解梦：这个梦的含义是，"
     content = dream + " 周公解梦：这个梦的含义是,"
     body = {
-        "token": TOKEN,
+        "token": utils.TOKEN,
         "app": "chat",
         "content": content
     }
@@ -131,7 +88,7 @@ def interpret_dream(request):
     # print("content len: " + str(len(content)))
     data = bytes(json.dumps(body), 'utf8')
     headers = {"Content-Type": 'application/json'}
-    req = urllib.request.Request(url=URL_GPT, headers=headers, data=data)
+    req = urllib.request.Request(url=utils.URL_GPT, headers=headers, data=data)
 
     # 同时多次请求
     try:
@@ -153,7 +110,7 @@ def interpret_dream(request):
         interpret = delBadSentence(interpret)
         if interpret == "":
             return HttpResponse("此梦境前无古人后无来者，简直太厉害了。")
-        insert_db(dream, interpret, "")
+        db.insert_db(dream, interpret, "")
         # get_db(dream)
         return HttpResponse(interpret)
     except Exception as e:
@@ -161,16 +118,30 @@ def interpret_dream(request):
         print("error happened!")
         return HttpResponse("error happened!")
 
+def similar_dream(request):
+    if request.method == 'POST':
+        try:
+            req = json.loads(request.body)
+            dream = req.get('dream')
+            # print("收到梦境 " + dream)
+        # return_json = json.dumps((name, job))
+        # return HttpResponse(return_json)
+        except Exception as e:
+            print(e)
+    else:
+        return_json = 'POST only!'
+        return HttpResponse(return_json)
+
 
 def check_times(request):
     body = {
-        "token": TOKEN
+        "token": utils.TOKEN
     }
     data = json.dumps(body)
     data = bytes(data, 'utf8')
     print(data)
     headers = {"Content-Type": 'application/json'}
-    req = urllib.request.Request(url=URL_USER, headers=headers, data=data)
+    req = urllib.request.Request(url=utils.URL_USER, headers=headers, data=data)
     try:
         resp = urllib.request.urlopen(req).read()
         print(resp.decode('utf-8'))

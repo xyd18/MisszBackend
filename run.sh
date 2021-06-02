3c4a53c28025a734196615b95ff8facef45336fe
luojj <jingjia_luo@163.com>
2021-06-03 04:07:53 +0800

    虽然run.sh不会被运行，但是把需要在missz底下建一个media文件夹写进去了，不然会报错

--- run.sh
+++ run.sh
@@ -1,7 +1,10 @@
 cd missz
 #python3 manage.py makemigrations dream
 #python3 manage.py migrate
-pip3 install -r ./requirements.txt
+if [ ! -d "media" ]; then
+  mkdir media
+fi
+pip3 install -r ../requirements.txt
 echo "finish install requirements.txt"
 # nohup python3 missz/manage.py runserver 0.0.0.0:8000 > backend.log 2>&1 &
 # echo "runserver"
